50bf12b556819cac31a6e8ba3ad6424e3af439a1
Erik Salinas <salinas.erik2002@gmail.com>
2023-04-20 12:56:35 -0300

    JAVASCRIPT INICIO

---
+++ javascript/script.js
@@ -0,0 +1,53 @@
+/*Primeraa forma de comentar*/ 
+//Segunda forma de comentar
+
+//*VARIABLES
+//Opcion 1
+//!VAR NO SE USA!!!!!!!!
+var nombre = "Mario"; //texto (string)
+//?Recomendado
+let apellido = "Torres"; //texto (string)
+//varibale, no se puede modificar
+const dniPersonalizado= 233528172 ; //numero (number)
+
+//Opcion 2
+let gato ,perro ,elefaante;
+gato = "Gato pepe",
+perro = "Perro pepe",
+elefaante = "Juan";
+
+
+//*Verificamos información
+/* console.log(nombre);
+console.log(apellido);
+console.log(dniPersonalizado); */
+
+
+
+//--------------------------------------------------------------------------------------------
+
+let number1 = 5;
+let number2 = 10;
+const number3 = 20;
+//Suma
+let suma = number1 + number2;
+console.log(suma);
+
+//Resta
+let resta = number1 - number3 + number2;
+console.log(resta);
+
+//Multiplicación
+let mult = number3 * number1;
+console.log(mult); 
+
+let nombre2 = "Roco";
+let texto = "Welcomen";
+const BLANCO = " ";
+/* let a = texto + BLANCO +nombre2; */
+/* let a = texto + " " +nombre2; */
+let a = `${texto} ${nombre2}  a tu pagina web`;
+
+
+console.log(a);
+
